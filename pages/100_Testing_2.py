33c2ff783ababfde3559f3a983bb4894c66381a1
Chaw Meng Fei <cmengfei78@gmail.com>
2023-08-07 16:05:11 +0800

    1

--- pages/100_Testing_2.py
+++ pages/100_Testing_2.py
@@ -1,13 +1,18 @@
 import os
+from pathlib import Path
+download_path = str(Path.home()/'Downloads')
+st.write(download_path)
 
-path = 'C:/FPS_Downloaded_Reports'
 
-# check whether directory already exists
-if not os.path.exists(path):
-    os.mkdir(path)
-    print("Folder %s created!" % path)
-else:
-    print("Folder %s already exists" % path)
+# path = 'C:/FPS_Downloaded_Reports'
+
+# # check whether directory already exists
+# if not os.path.exists(path):
+#     os.mkdir(path)
+#     print("Folder %s created!" % path)
+# else:
+#     print("Folder %s already exists" % path)
+
 
 # import requests
 
