e99001554b879da6c8ea46db4b1dfa2a68977a0a
Chaw Meng Fei <cengfei78@mail.com>
2023-08-09 16:04:45 +0800

    1

---
+++ assets/Database Tables.sql
@@ -0,0 +1,26 @@
+/*
+-- Tables --
+GMS_YYT1_FPSBatchStp
+GMS_YYT_FPSBatchStp
+
+FPS_YYT_BatchMthList
+FPS_YYT_BatchSuppList
+------------------------
+
+
+Select * from GMS_YYT_FPSBatchStp
+Select * from FPS_MthEndClose
+Select * from FPS_Proforma
+Select * from FPS_ProformaYOP
+Select * from FPS_ProformaSupplier
+Select * from FPS_ProformaFFBIncome
+Select * from FPS_ProformaTaxCont
+
+
+For Posting Record:
+Select * from FPS_FFBApplyPaymentHdr
+Select * from FPS_FFBApplyPaymentDet
+Select * from FPS_FinalPayHdr
+Select * from FPS_FinalPayDet
+
+*/
